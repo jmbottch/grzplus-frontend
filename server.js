c13aba13680929c3a05d0ee9307e4c3f7e922696
Jeroen Bottcher <jmbottch@avans.nl>
2021-04-08 15:01:19 +0200

    bugfix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(compression());
 
-const appname = 'GRZPLUSDashboard';
+const appname = 'grzplus';
 
 app.use(express.static(path.join(__dirname, 'dist', appname)));
 
